13eb91938f465b4980c0e29be35432006568f041
piyush97 <me@piyushmehta.com>
2023-11-09 19:28:04 -0500

    Setup

--- pub.py
+++ pub.py
@@ -2,6 +2,13 @@ from get_weather_data import parse_weather_data
 import paho.mqtt.client as mqtt
 import json
 import time
+import requests
+
+def get_ip_address():
+    url = 'https://api.ipify.org'
+    response = requests.get(url)
+    ip_address = response.text
+    return ip_address
 
 def on_connect(client, userdata, flags, rc):
     print("Publisher Connected with result code " + str(rc))
@@ -17,17 +24,40 @@ def client_interface():
     client.connect("mqtt.eclipseprojects.io", 1883)
     return client
 
+def publish_weather_info(client, topic, value):
+    # Get IP address and device name
+    ip_address = get_ip_address()
+    device_name = "piyush-pi"
+
+    # Format the time
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Create MQTT message
+    mqtt_message = {
+        "time": current_time,
+        "ip": ip_address,
+        "devicename": device_name,
+        "value": value
+    }
+
+    payload = json.dumps(mqtt_message)
+    client.publish(topic, payload)
+    print(f"Published new weather data to topic '{topic}'")
+    
 def publish():
     client = client_interface()
     api_response = parse_weather_data()
-    client.publish("weather", json.dumps(api_response))
-    print("Published new weather data to topic 'weather'")
+    publish_weather_info(client, "temperature", api_response["temperature"])
+    publish_weather_info(client, "humidity", api_response["humidity"])
+    publish_weather_info(client, "rain_status", api_response["raining"])
+    publish_weather_info(client, "wind_speed", api_response["windspeed"])
     client.loop_forever()
-
+    
 def main():
     while True:
         publish()
+        
         time.sleep(300)  # Sleep for 5 minutes
-
+    
 if __name__ == "__main__":
     main()
--- sub.py
+++ sub.py
@@ -1,24 +1,83 @@
 import paho.mqtt.client as mqtt
+import sys
+import json
 
 def on_connect(client, userdata, flags, rc):
     print("Subscriber connected with result code "+str(rc))
-
+    client.subscribe(sys.argv[1])
+    print('Subscribed to topic ' + sys.argv[1])
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    
+    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
+    save_data_using_sys(msg)
+    check_conditions(msg)
+
+def check_temperature_conditions(msg):
+    print("Warning: Temperature below 10 degrees for 30 consecutive minutes.")
+
+def check_wind_speed_conditions(msg):
+    print("Warning: Wind speed more than 20 km/hour for the last 10 minutes.")
+
+def check_rain_conditions(msg):
+    print("Warning: Rain continues for more than 45 hours.")
+
+def check_conditions(msg):
+    try:
+        payload_data = json.loads(msg.payload.decode("utf-8"))
+
+        temperature = payload_data.get("temperature", None)
+        wind_speed = payload_data.get("wind_speed", None)
+        rain_duration = payload_data.get("rain_duration", None)
+
+        if temperature is not None and temperature < 10:
+            check_temperature_conditions(msg)
+
+        if wind_speed is not None and wind_speed > 20:
+            check_wind_speed_conditions(msg)
+
+        if rain_duration is not None and rain_duration > 45:
+            check_rain_conditions(msg)
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except KeyError as e:
+        print(f"KeyError: {e}. Make sure the payload has all required fields.")
+
+def save_data_using_sys(msg):
+    try:
+        payload_data = json.loads(msg.payload.decode("utf-8"))
+
+        time = payload_data["time"]
+        ip = payload_data["ip"]
+        devicename = payload_data["devicename"]
+        value = payload_data["value"]
+
+        formatted_data = f"{time} {value}"
+
+        topic = sys.argv[1]
+        file_name = topic + ".txt"
+
+        with open(file_name, "a") as f:
+            f.write(formatted_data + "\n")
+            print("Data saved to file " + file_name)
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except KeyError as e:
+        print(f"KeyError: {e}. Make sure the payload has all required fields.")
+
 def client_interface():
-    
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
 
     client.connect("mqtt.eclipseprojects.io", 1883)
-    client.subscribe("weather")
-    print("Subscribed to topic 'weather'")
-    client.loop_forever()
-    return client
-    
 
-    
-client_interface()
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        print("Disconnecting gracefully...")
+        client.disconnect()
+
+if __name__ == "__main__":
+    client_interface()
