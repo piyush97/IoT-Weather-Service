a6d60f458faae5209bf45e6d8e2c2201693ada81
piyush97 <me@piyushmehta.com>
2023-11-09 19:34:50 -0500

    Fixes

--- pub.py
+++ pub.py
@@ -1,21 +1,27 @@
+
+# Import the necessary libraries
 from get_weather_data import parse_weather_data
 import paho.mqtt.client as mqtt
 import json
 import time
 import requests
 
+# Get the IP address of the device
 def get_ip_address():
     url = 'https://api.ipify.org'
     response = requests.get(url)
     ip_address = response.text
     return ip_address
 
+# Handle the connection to the MQTT broker
 def on_connect(client, userdata, flags, rc):
     print("Publisher Connected with result code " + str(rc))
 
+# Handle the receipt of a message
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
+# Connect to the MQTT broker
 def client_interface():
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -24,6 +30,7 @@ def client_interface():
     client.connect("mqtt.eclipseprojects.io", 1883)
     return client
 
+# Publish the weather data to the MQTT broker
 def publish_weather_info(client, topic, value):
     # Get IP address and device name
     ip_address = get_ip_address()
